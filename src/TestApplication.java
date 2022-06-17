425796146796106cc0cd2dd3659dc44f7c35e198
Paulo Paulo Dehon Nobre de Araujo F <paraujo@server01.foursys.local>
2022-06-17 08:31:43 -0300

    first commit at novaBranch

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -1,5 +1,9 @@
 public class TestApplication {
     public static void main(String[] args) {
-        System.out.println("Primeira alteração na branch main");
+        System.out.println(metodo2());
+    }
+
+    public static String metodo2() {
+        return "Primeira alteração na segunda branch";
     }
 }
