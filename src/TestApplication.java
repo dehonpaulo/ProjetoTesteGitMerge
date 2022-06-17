40ea849e6f5c0f0150353119523743cb9bf8fed2
Paulo Dehon <60795308+dehonpaulo@users.noreply.github.com>
2022-06-17 08:28:52 -0300

    segunda alteração na main

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -1,5 +1,10 @@
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Primeira alteração na branch main");
+        System.out.println(metodo1);
+    }
+    
+    public static String metodo1() {
+        return "Segunda alteração na branch main";
     }
 }
